8d5da9c1af7f4967e6e73570c2f7893c6e441193
Alan <alancharlescramer@gmail.com>
2015-10-08 14:43:26 -0400

    refactor

---
+++ makeBoard.js
@@ -0,0 +1,43 @@
+
+MyApp.makeBoard = function (boxWidth, boxHeight, pxPerIn, color, fjStarts) {
+
+    var board = svg.append("g");
+    
+    board.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", boxWidth*pxPerIn)
+        .attr("height", boxHeight*pxPerIn)
+        .style("fill", color)
+        ;
+    
+    var fingerJts = MyApp.fingerJoint()
+        .fingerWidth(.5*pxPerIn)
+        .fingerHeight(.25*pxPerIn) // quarter inch material
+        .radius(.08*pxPerIn) // eighth inch diam bit
+        ; // returns function 
+    
+    var lfjs = board.selectAll(".leftFingerJoint")
+        .data(fjStarts);
+          
+    lfjs.enter()
+        .append("g")
+        .attr("class", "leftFingerJoint")
+        .attr("transform", "translate (0,0) rotate(-90)" )
+        .call(fingerJts)
+        ;
+        
+    var rfjs = board.selectAll(".rightFingerJoint")
+        .data(fjStarts);
+          
+    rfjs.enter()
+        .append("g")
+        .attr("class", "rightFingerJoint")
+        .attr("transform", "translate (" + 
+            (boxWidth*pxPerIn) + ", " + 
+            (boxHeight*pxPerIn) + ") rotate(90)" )
+        .call(fingerJts)
+        ;        
+
+    return board;
+}
